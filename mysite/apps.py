0233610d5f2f3333c26424f05c6453e26f61d1ff
shaocloud <shao.cloud@gmail.com>
2019-04-11 19:17:00 +0800

    Add verbose name

--- mysite/apps.py
+++ mysite/apps.py
@@ -6,6 +6,7 @@ from django.apps import AppConfig as BaseAppConfig
 class AppConfig(BaseAppConfig):
 
     name = "mysite"
+    verbose_name = "CS4242 Mini-Project Site"
 
     def ready(self):
         import_module("mysite.receivers")
