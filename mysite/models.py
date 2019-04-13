795dfdb919eff84cd831987bf7360ba89e862c7c
shaocloud <shao.cloud@gmail.com>
2019-04-13 20:22:02 +0800

    Make pageURL primary key

--- mysite/models.py
+++ mysite/models.py
@@ -31,10 +31,10 @@ class Subcat(models.Model):
 
 class Article(models.Model):
     """Model representing a specific article."""
+    #the url is the key for the article
+    page_url = models.URLField(primary_key = True, help_text='Link to Article', null = False)
+
     # Meta
-    # Foreign Key used because article can only have one author, but authors can have multiple articles
-    # Author as a string rather than object because it hasn't been declared yet in the file
-    id = models.AutoField(primary_key = True)
     title = models.CharField(max_length=200, help_text='Article name')
     author = models.CharField(max_length=200, help_text='Author name', null = True)
     summary = models.TextField(help_text='The given synopsis for the file', null = True)
@@ -49,13 +49,11 @@ class Article(models.Model):
     SITES = [("IN","Instructables"),
             ("WH","WikiHow")]
     sitetype = models.CharField(max_length = 200, help_text='Instructables or WikiHow', choices=SITES, default="1", null = True)
-    page_url = models.URLField(help_text='Link to Article', null = True)
     img = models.URLField(help_text='Link to image', null = True)
 
     # categories
     cat = models.ForeignKey(Category, on_delete=models.SET_NULL, help_text='Category this article is related to', null = True)
     subcat = models.ManyToManyField(Subcat, help_text='Subcategories this article is related to')
-
 
     def __str__(self):
         """String for representing the Model object."""
