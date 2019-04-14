4bd2c32a89525051b9cbc01bb46b551b6ba81986
shaocloud <shao.cloud@gmail.com>
2019-04-14 14:36:14 +0800

    Add functions (--shrink_in and --convert_ast) to manipulate jsons

--- mysite/management/commands/load_json.py
+++ mysite/management/commands/load_json.py
@@ -1,4 +1,5 @@
 import json
+import ast
 import os, sys
 import django
 from progress.bar import IncrementalBar
@@ -24,7 +25,9 @@ class Command(BaseCommand):
         parser.add_argument('--example', action='store_true', help='creates an example category')
         parser.add_argument('--store_wh', action='store_true', help='loads wikihow.json')
         parser.add_argument('--store_in', action='store_true', help='loads instructables.json')
+        parser.add_argument('--shrink_in', action='store_true', help='shrinks instructables.json')
         parser.add_argument('--store_all', action='store_true', help='loads both wikihow.json and instructables.json')
+        parser.add_argument('--convert_ast', action='store_true', help='converts txt to json')
 
     def _get_wh_file(self, filepath):
         return proc.load_wh(filepath)
@@ -48,11 +51,15 @@ class Command(BaseCommand):
             summary = entry["summary"],     \
             datetime = entry["date"],       \
             cat = entry["category"])
-#            wh_record.cat = entry["category"]
+
             if not created:
                 wh_record.save()
             else:
                 skipped += 1
+
+            for subcat in entry["sub_categories"]:
+                subcat.article_set.add(wh_record)
+
             bar.next()
         bar.finish()
         if skipped:
@@ -66,6 +73,36 @@ class Command(BaseCommand):
         new_art = Article(  title="example",
                             author="example")
         new_art.save()
+
+    def _conv(self, inname, outdir):
+        with open(inname) as file_to_conv:
+            values = ast.literal_eval(file_to_conv.read())
+            file_to_conv.close()
+        outname = input("Save converted %s as: " % inname)
+        with open(outdir + outname, 'w') as out_file:
+            json.dump(values,out_file)
+            out_file.close()
+
+    def _shrink(self,IN_name,IN_out):
+        with open(IN_name) as file_to_shrink:
+            data_IN = json.load(file_to_shrink)
+            bar = IncrementalBar("Shrinking raw_text:", max = len(data_IN))
+            new_data_IN = []
+            new_entry = {}
+            for entry in data_IN:
+                new_entry = entry
+                new_entry["raw_text"] = proc.shorten(''.join(entry["raw_text"]))
+                new_data_IN.append(new_entry)
+                bar.next()
+            bar.finish()
+        file_to_shrink.close()
+        with open(IN_out, 'w') as out_file:
+            print("Writing to %s" % IN_out)
+            json.dump(new_data_IN, out_file)
+        out_file.close()
+        print("%s is %d bytes" % (IN_name, os.path.getsize(IN_name)))
+        print("%s is %d bytes" % (IN_out, os.path.getsize(IN_out)))
+        print("%.2f percent reduction wew" % os.path.getsize(IN_out)/os.path.getsize(IN_name))
 
     def handle(self, *args, **options):
         #fields
@@ -97,3 +134,11 @@ class Command(BaseCommand):
             print("Loading from " + IN_FILENAME + ": ")
             IN_data = self._get_in_file(DIR_NAME+IN_FILENAME)
             print("%d records found for %s" % (len(IN_data),IN_FILENAME))
+        elif options["convert_ast"]:
+            print("Converting wikihow.txt:")
+            self._conv(DIR_NAME+"wikihow.txt",DIR_NAME)
+            print("Converting instructables.txt:")
+            self._conv(DIR_NAME+"instructables.txt",DIR_NAME)
+        elif options["shrink_in"]:
+            print("Shrinking instructables_large.json: ")
+            self._shrink(DIR_NAME + "instructables_large.json", DIR_NAME + IN_FILENAME)
